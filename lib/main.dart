e6d1590efd77e38759710d6e0366f6c4393c4395
bagh98~ <abonaorz@gmail.com>
2025-02-02 19:07:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ebook_app/Features/Splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooksApp());
+}
+
+class BooksApp extends StatelessWidget {
+  const BooksApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
