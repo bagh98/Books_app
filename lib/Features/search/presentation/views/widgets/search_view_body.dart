7e9b28c701d2ba1440fde80d3d4765b5fe4b4c61
bagh98~ <abonaorz@gmail.com>
2025-02-10 22:30:27 +0300

    search view done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:ebook_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:ebook_app/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:ebook_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -6,12 +8,40 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: SearchResultView())
         ],
+      ),
+    );
+  }
+}
+
+class SearchResultView extends StatelessWidget {
+  const SearchResultView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: BookListViewItem(),
       ),
     );
   }
