a7a998767bc2272611d932b486a44b6c1c348e13
bagh98~ <abonaorz@gmail.com>
2025-02-13 22:49:09 +0300

    create featured books  method

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_state.dart
@@ -14,11 +14,11 @@ final class FeaturedBooksLoading extends FeaturedBooksState {}
 final class FeaturedBooksFailure extends FeaturedBooksState {
   final String errMessage;
 
-  const FeaturedBooksFailure({required this.errMessage});
+  const FeaturedBooksFailure(this.errMessage);
 }
 
 final class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
 
-  const FeaturedBooksSuccess({required this.books});
+  const FeaturedBooksSuccess(this.books);
 }
